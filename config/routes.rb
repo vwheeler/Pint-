6f0283816ad77b0af68f3166d36005d311bffae3
Vincent Wheeler <vincentwheelr@gmail.com>
2013-10-22 17:20:43 -0600

    added bootstrap and customization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pint::Application.routes.draw do
   root 'pages#home'
-  get "about" => 'pages#about' # creates about path 
+  get "about" => 'pages#about'
+  get "mission" => 'pages#mission'  # creates about path 
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
